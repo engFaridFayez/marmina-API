3303a319312fcdb77ee3e5a78a0bd3d49507157d
Farid <faridstage@gmail.com>
2025-09-21 17:13:05 +0300

    error in updateuser

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 
 from .custom_jwt_claims import CustomTokenObtainPairView
 
-from users.views import UsersView, NewUserView
+from users.views import DeactivateUserView, DeleteUserView, UpdateUserInfo, UpdateUserStatusView, UsersView, NewUserView
 
 from rest_framework.routers import DefaultRouter
 
@@ -13,6 +13,10 @@ urlpatterns = [
     path('', include(router.urls)),
 
     path('users/new',NewUserView.as_view(),name='new_user'),
-    path('token/',CustomTokenObtainPairView.as_view(),name='token_obtain_pair')
+    path('token/',CustomTokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('users/delete',DeleteUserView.as_view(),name='delete_user'),
+    path('users/deactivate',DeactivateUserView.as_view(),name='deactivate_user'),
+    path('users/update-activity-status',UpdateUserStatusView.as_view(),name='update_activity_status'),
+    path('users/update-user-info/<int:id>',UpdateUserInfo.as_view(),name='update_user_info')
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.utils import timezone
-from rest_framework import permissions
+from rest_framework import permissions , status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -15,6 +15,21 @@ class UsersView(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     paginator = None
+
+class UpdateUserInfo(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self,request,format=None,*args,**kwargs):
+        data = request.data
+        user = User.objects.get(id=kwargs['id'],username=data['username'],first_name=data['firstName'],last_name=data['lastName'],email=data['email'])
+        status = 201
+        try:
+            user.save()
+        except Exception:
+            error_message = User._meta.get_field('username').error_messages['unique']
+            status = 406
+            return Response({'error':error_message},status=status)
+        return Response({'User Updated!'},status=200)
 
 class NewUserView(APIView):
 
@@ -33,3 +48,35 @@ class NewUserView(APIView):
             return Response({'error':error_message},status=status)
         
         return Response({"User Created Successfully!!"},status=status)
+    
+class DeleteUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated,permissions.IsAdminUser]
+
+    def delete(self,request,format=None):
+        data = request.data
+        user = User.objects.get(id=data['user_id'])
+        user.delete()
+        return Response({},status=200)
+    
+class DeactivateUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated,permissions.IsAdminUser]
+
+    def post(self,request):
+        data = request.data
+        user = User.objects.get(username=data['username'])
+        user.is_active = False
+        user.save()
+        return Response({"User has been deactivated successfully!"},status=status.HTTP_200_OK)
+    
+class UpdateUserStatusView(APIView):
+    permission_classes = [permissions.IsAuthenticated,permissions.IsAdminUser]
+
+    def post(self,request):
+        data = request.data
+        username = data['username']
+        activity = data['activity']
+
+        user = User.objects.get(username=username)
+        user.is_active = activity
+        user.save()
+        return Response({"Activity updated"},status=status.HTTP_200_OK)
