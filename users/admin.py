32945c429190ee23bca949e170f9fcf3829f9a2e
Farid <faridfaiz1@outlook.com>
2026-09-02 16:19:41 +0300

    display full_name in axes attempts

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 from users.models import CustomUser
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from axes.models import AccessAttempt
+from axes.admin import AccessAttemptAdmin
 
+User = get_user_model()
+
+admin.site.unregister(AccessAttempt)
+
+
+class CustomAccessAttemptAdmin(AccessAttemptAdmin):
+    list_display = list(AccessAttemptAdmin.list_display) + ['get_full_name']
+
+    def get_full_name(self, obj):
+        user = User.objects.filter(username=obj.username).first()
+        return user.full_name if user else "-"
+    get_full_name.short_description = "Full Name"
+
+
+admin.site.register(AccessAttempt, CustomAccessAttemptAdmin)
 # Register your models here.
 admin.site.register(CustomUser)
